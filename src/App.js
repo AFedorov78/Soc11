1d3173fb875525673daf3caac54b6dff3f938d22
Andrey <andrey.fedorov1978@yndex.ru>
2022-01-09 16:21:45 +0300

    new App 5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import React, {  useState } from 'react';
-import './App.css';
+//import './App.css';
 import axios from 'axios';
 
 
